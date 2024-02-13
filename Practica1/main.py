17a0a819249a3ccc24eb5e37278e87826de28e57
valentinagbf <153018573+valentinagbf@users.noreply.github.com>
2024-02-13 19:05:12 +0100

    Update main.py

--- Practica1/main.py
+++ Practica1/main.py
@@ -7,38 +7,41 @@ from hotel import *
 
 comando = sys.argv
 if len(comando) > 1:
-    if comando[1].lower() == "afegir" and comando[2].lower() == "habitacio":
-        if comprobarLong(comando, 6):
-            num = comando[3]
-            capacidad = comando[4]
-            precio = comando[5]
-            afegirHabitacio(num, capacidad, precio)
-    elif comando[1].lower() == "afegir" and comando[2].lower() == "reserva":
-        if comprobarLong(comando, 8):
-            num = comando[3]
-            nom = comando[4]
-            ap = comando[5]
-            dni = comando[6]
-            tel = comando[7]
-            afegirReserva(num, nom, ap, dni, tel)
-    elif comando[1].lower() == "finalitzar":
-        if comprobarLong(comando, 4):
-            num = comando[2]
-            dias = comando[3]
-            finalitzarHabitacio(num, dias)
-    elif comando[1].lower() == "reserves":
-        if comprobarLong(comando, 2):
-            mostrarReservas()
-    elif comando[1].lower() == "netejar":
-        if comprobarLong(comando,3):
-            num = comando[2]
-            netejarHabitacio(num)
-    elif comando[1].lower() == "info":
-        if comprobarLong(comando, 3):
-            dni = comando[2]
-            info(dni)
-    elif comando[1].lower() == "list":
-        if comprobarLong(comando, 2):
-            list()
+    if len(comando) > 2:
+        if comando[1].lower() == "afegir" and comando[2].lower() == "habitacio":
+            if comprobarLong(comando, 6):
+                num = comando[3]
+                capacidad = comando[4]
+                precio = comando[5]
+                afegirHabitacio(num, capacidad, precio)
+        elif comando[1].lower() == "afegir" and comando[2].lower() == "reserva":
+            if comprobarLong(comando, 8):
+                num = comando[3]
+                nom = comando[4]
+                ap = comando[5]
+                dni = comando[6]
+                tel = comando[7]
+                afegirReserva(num, nom, ap, dni, tel)
+        elif comando[1].lower() == "finalitzar":
+            if comprobarLong(comando, 4):
+                num = comando[2]
+                dias = comando[3]
+                finalitzarHabitacio(num, dias)
+        elif comando[1].lower() == "reserves":
+            if comprobarLong(comando, 2):
+                mostrarReservas()
+        elif comando[1].lower() == "netejar":
+            if comprobarLong(comando,3):
+                num = comando[2]
+                netejarHabitacio(num)
+        elif comando[1].lower() == "info":
+            if comprobarLong(comando, 3):
+                dni = comando[2]
+                info(dni)
+        elif comando[1].lower() == "list":
+            if comprobarLong(comando, 2):
+                list()
     else:
-        print("Comando Incorrecto.")
+        print("Comando Incorrecto. Prueba otra vez")
+else:
+    print("Comando Incorrecto. Prueba otra vez")
